3514ef6cb2bd4c6b76331e5e80fe9a72631cdded
Luis Antônio <tyluis20@gmail.com>
2018-05-24 17:58:21 -0300

    criando migrações entre jobs e o manage

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+#jobs area (herdando de models) 
+class Job(models.Model):
+    imagefiles = models.ImageField(upload_to='imagens/')
+    sumary = models.CharField(max_length=200)
