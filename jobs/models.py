81d3c3f286bc595ba12fcb888375d82b9d06f19f
Luis Antônio <tyluis20@gmail.com>
2018-05-24 23:09:03 -0300

    adicionando files

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 #jobs area (herdando de models) 
 class Job(models.Model):
-    imagefiles = models.ImageField(upload_to='imagens/')
+    image = models.ImageField(upload_to='imagens/')
     sumary = models.CharField(max_length=200)
