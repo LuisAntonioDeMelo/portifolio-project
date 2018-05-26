5248e5bf8e73fc94b523dc30004f56f6adc5cd30
Luis Antônio <tyluis20@gmail.com>
2018-05-25 21:53:02 -0300

    funcionando em  parte ainda falta as views

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=60)
+    imagem = models.ImageField(upload_to='imagens/')
+    description = models.CharField(max_length=200)
